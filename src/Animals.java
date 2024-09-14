1f38fcb69293f28e2acefd4924ee66cf9c84e03a
fant0m23 <fant0m2311@gmail.com>
2024-09-14 23:09:56 +0300

    initial

---
+++ src/Animals.java
@@ -0,0 +1,11 @@
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+
+public class Animals {
+    private String name;
+    private LocalDateTime birthdate;
+    private HashSet<String> execCommands;
+
+
+}
