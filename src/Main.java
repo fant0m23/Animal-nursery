44eb244d7ac02815642fa04be8a42c943b8d4769
fant0m23 <fant0m2311@gmail.com>
2024-09-22 14:26:50 +0300

    t

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashSet<String> set = new HashSet<>();
+        set.add("сидеть");
+        set.add("лежать");
+
+        Animals one = new Animals("Boss", LocalDateTime.now(), set);
+
+        System.out.println(one);
     }
 }
